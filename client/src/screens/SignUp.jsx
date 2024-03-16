8e59e0f4d54839f8ef0c7a5c5f5bfa90bdb7f44f
Sanil-Surve <sanilsurve42@gmail.com>
2024-03-16 23:27:55 +0530

    initial commit

--- client/src/screens/SignUp.jsx
+++ client/src/screens/SignUp.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const SignUp = () => {
-  return (
-    <div>
-      SignUp
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className="container">
+                <div className="register-container">
+                    <form className="register-form" action="register.php" method="POST">
+                        <h2>Register</h2>
+                        <div className="input-group">
+                            <label htmlFor="fullname">Full Name:</label>
+                            <input type="text" id="fullname" name="fullname" required />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="username">Username:</label>
+                            <input type="text" id="username" name="username" required />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                        <button type="submit">Register</button>
+                    </form>
+                    <button className="login-button">Go to Login</button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default SignUp
+export default SignUp;
