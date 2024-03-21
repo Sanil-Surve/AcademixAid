b75b29840d251ce68cd25615438ab83ea0b57bed
Sanil-Surve <sanilsurve42@gmail.com>
2024-03-22 02:31:55 +0530

    initial commit

--- server/middlewares/validation/user.js
+++ server/middlewares/validation/user.js
@@ -27,16 +27,6 @@ exports.validateUserSignUp = [
     .withMessage('Password is empty!')
     .isLength({ min: 8, max: 20 })
     .withMessage('Password must be 3 to 20 characters long!'),
-  // check('confirmPassword')
-  //   .trim()
-  //   .not()
-  //   .isEmpty()
-  //   .custom((value, { req }) => {
-  //     if (value !== req.body.password) {
-  //       throw new Error('Both password must be same!');
-  //     }
-  //     return true;
-  //   }),
 ];
 
 exports.userVlidation = (req, res, next) => {
